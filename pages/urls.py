3bcf3a9f89cbf274b215f4159ea0beb8fbabc180
mcgridles <henry.gridley@gmail.com>
2017-04-19 11:51:50 -0400

    fixed detail view

--- pages/urls.py
+++ pages/urls.py
@@ -4,8 +4,15 @@ from . import views
 app_name = 'pages'
 urlpatterns = [
     #url(r'^$', views.index, name='index'),
+    # /pages/work/
     url(r'^work/$', views.WorkView.as_view(), name='work'),
+    # /pages/work/#/
+    url(r'^work/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
+    # /pages/projects/
     url(r'^projects/$', views.ProjectView.as_view(), name='projects'),
-    url(r'^aboutMe/$', views.aboutMe, name='aboutMe'),
-    url(r'^details(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details')
+    # /pages/projects/#/
+    url(r'^projects/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
+    # /pages/aboutMe/
+    url(r'^aboutMe/$', views.aboutMe, name='aboutMe')
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.utils import timezone
+from django.http import HttpResponse
 
 from .models import Author, ExpPost
 
@@ -14,8 +15,7 @@ class WorkView(generic.ListView):
         """Return all relevant experiences"""
         return ExpPost.objects.filter(
             pub_date__lte=timezone.now()
-            ).filter(
-            start_date__lte=timezone.now()
+            ).filter(start_date__lte=timezone.now()
             ).exclude(post_type='project').order_by('-start_date')
 
 class ProjectView(generic.ListView):
@@ -26,14 +26,12 @@ class ProjectView(generic.ListView):
         """Return all relevant projects"""
         return ExpPost.objects.filter(
             pub_date__lte=timezone.now()
-            ).filter(
-            start_date__lte=timezone.now()
+            ).filter(start_date__lte=timezone.now()
             ).filter(post_type='project').order_by('-start_date')
 
 class DetailView(generic.DetailView):
     model = ExpPost
     template_name = 'pages/details.html'
-    context_object_name = 'post_detail'
 
 def index(request):
     return render(request, 'pages/index.html')
