02f01aee479b6fa1fa87366cfbc8d189b00b9de5
mcgridles <henry.gridley@gmail.com>
2017-04-19 11:51:11 -0400

    added image and journal models

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import ExpPost, Author
+from .models import Author, ExpPost, Image, Journal
 
-admin.site.register(ExpPost)
 admin.site.register(Author)
+admin.site.register(ExpPost)
+admin.site.register(Image)
+admin.site.register(Journal)
--- pages/models.py
+++ pages/models.py
@@ -13,15 +13,6 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
-class ImageModel(models.Model):
-    title = models.CharField(max_length=50)
-    img = models.ImageField()
-
-    def __str__(self):
-        return self.title
-
-#class Journal(models.Model):
-
 class ExpPost(models.Model):
     title = models.CharField(max_length=255)
     position = models.CharField(max_length=255, null=True, blank=True)
@@ -30,7 +21,24 @@ class ExpPost(models.Model):
     start_date = models.DateField('date work started', null=True)
     end_date = models.DateField('date work ended', null=True, blank=True)
     description = models.TextField()
-    images = models.ManyToManyField(ImageModel, blank=True)
+
+    def __str__(self):
+        return self.title
+
+class Image(models.Model):
+    post = models.ForeignKey(ExpPost)
+    title = models.CharField(max_length=50)
+    img = models.ImageField()
+
+    def __str__(self):
+        return self.title
+
+class Journal(models.Model):
+    post = models.ForeignKey(ExpPost)
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField('date published')
+    author = models.CharField(max_length=50)
+    text = models.TextField(null=True)
 
     def __str__(self):
         return self.title
