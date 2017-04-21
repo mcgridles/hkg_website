4289c01267f42be7d633ba1757325196f18a2460
mcgridles <henry.gridley@gmail.com>
2017-04-20 22:05:23 -0400

    cleaned up

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+
 from . import views
 
 app_name = 'pages'
@@ -7,11 +8,11 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # /pages/work/
     url(r'^work/$', views.WorkView.as_view(), name='work'),
-    # /pages/work/#/
+    # /pages/work/<pk>/
     url(r'^work/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
     # /pages/projects/
     url(r'^projects/$', views.ProjectView.as_view(), name='projects'),
-    # /pages/projects/#/
+    # /pages/projects/<pk>/
     url(r'^projects/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
     # /pages/aboutMe/
     url(r'^aboutMe/$', views.aboutMe, name='aboutMe'),
