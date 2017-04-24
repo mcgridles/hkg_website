acaab93fb7e4f24fb062abed0caf83790688364e
mcgridles <henry.gridley@gmail.com>
2017-04-23 20:16:32 -0400

    fixed static root

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -124,7 +124,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "pages/static"),
+    #'/var/www/static/',
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
