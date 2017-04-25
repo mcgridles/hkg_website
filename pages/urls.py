8bf53dbe092bb9da8af14aeaa5482e0b4c1aa26f
mcgridles <henry.gridley@gmail.com>
2017-04-25 00:14:31 -0400

    changed details urls

--- pages/urls.py
+++ pages/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
     # /pages/work/
     url(r'^work/$', views.WorkView.as_view(), name='work'),
     # /pages/work/<pk>/
-    url(r'^work/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
+    url(r'^work/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='work_details'),
     # /pages/projects/
     url(r'^projects/$', views.ProjectView.as_view(), name='projects'),
     # /pages/projects/<pk>/
-    url(r'^projects/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),
+    url(r'^projects/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='project_details'),
     # /pages/aboutMe/
     url(r'^aboutMe/$', views.aboutMe, name='aboutMe'),
 
