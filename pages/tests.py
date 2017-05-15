2b2161845a999cdf362310956555f5853f4a9ead
mcgridles <henry.gridley@gmail.com>
2017-05-15 14:18:58 -0400

    updated tests

--- pages/tests.py
+++ pages/tests.py
@@ -14,24 +14,31 @@ def create_author(name, age):
     """
     Creates a site author with the given 'name'
     """
-    return Author.objects.create(name=name, age=age)
+    return Author.objects.create(name=name, age=age, picture='hkg_portrait.png')
 
-class AuthorViewTests(TestCase):
-    def test_aboutMe_view_with_no_author(self):
+class HomepageViewTests(TestCase):
+    def test_homepage_view_with_no_author(self):
         """
         If no author exists, an appropriate message should be displayed.
         """
-        response = self.client.get(reverse('pages:aboutMe'))
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 404)
 
-    def test_aboutMe_with_author(self):
+    def test_homepage_with_author(self):
         """
         Author should be displayed on the page.
         """
-        name = 'Henry Gridley'
-        create_author(name=name, age=20)
-        response = self.client.get(reverse('pages:aboutMe'))
-        self.assertContains(response, name)
+        create_author(name='Henry Gridley', age=20)
+        response = self.client.get(reverse('home'))
+        self.assertContains(response, 'Henry Gridley')
+
+    def test_author_aspect_ratio(self):
+        """
+        Returns the aspect ratio of a given image.
+        """
+        create_author(name='Henry Gridley', age=20)
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.context['author'].aspect_ratio(), 0.921875)
 
 def create_post(title, post_type, days=-1, start=-1):
     """
@@ -42,164 +49,88 @@ def create_post(title, post_type, days=-1, start=-1):
     pub_time = timezone.now() + datetime.timedelta(days=days)
     start_time = timezone.now() + datetime.timedelta(days=start)
     return ExpPost.objects.create(title=title, post_type=post_type,
-        pub_date=pub_time, start_date=start_time)
+        pub_date=pub_time, start_date=start_time,
+        marquee='hkg_portrait.png')
 
-class WorkPostViewTests(TestCase):
-    def test_work_view_with_no_posts(self):
+class ListViewTests(TestCase):
+    def test_list_view_with_no_posts(self):
         """
-        If no work post exists, an appropriate message should be displayed.
+        If no post exists, an appropriate message should be displayed.
         """
         response = self.client.get(reverse('pages:work'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "No posts were found.")
-        self.assertQuerysetEqual(response.context['work_list'], [])
+        self.assertQuerysetEqual(response.context['post_list'], [])
 
-    def test_work_view_with_past_post(self):
+    def test_list_view_with_past_post(self):
         """
-        Work posts with a pub_date in the past should be displayed on the page.
+        Posts with a pub_date in the past should be displayed on the page.
         """
         create_post(title='Past post', post_type='work', days=-10)
         response = self.client.get(reverse('pages:work'))
         self.assertQuerysetEqual(
-            response.context['work_list'],
+            response.context['post_list'],
             ['<ExpPost: Past post>']
         )
 
-    def test_work_view_with_future_post(self):
+    def test_list_view_with_future_post(self):
         """
-        Work posts with a pub_date in the future should not be displayed.
+        Posts with a pub_date in the future should not be displayed.
         """
         create_post(title='Future post', post_type='work', days=10)
         response = self.client.get(reverse('pages:work'))
-        self.assertQuerysetEqual(response.context['work_list'], [])
+        self.assertQuerysetEqual(response.context['post_list'], [])
         self.assertContains(response, 'No posts were found.')
 
-    def test_work_view_with_future_and_past_post(self):
+    def test_list_view_with_future_and_past_post(self):
         """
-        Work posts with a pub_date in the future should not be displayed but
+        Posts with a pub_date in the future should not be displayed but
         posts with a pub_date in the past should be displayed.
         """
         create_post(title='Future post', post_type='work', days=10)
         create_post(title='Past post', post_type='work', days=-10)
         response = self.client.get(reverse('pages:work'))
         self.assertQuerysetEqual(
-            response.context['work_list'],
+            response.context['post_list'],
             ['<ExpPost: Past post>']
         )
 
-    def test_work_view_with_multiple_past_posts(self):
+    def test_list_view_with_multiple_past_posts(self):
         """
-        More than one work post can be displayed at a time as long as the pub_date
+        More than one post can be displayed at a time as long as the pub_date
         is in the past.
         """
         create_post(title='Past post 1', post_type='work', days=-10)
         create_post(title='Past post 2', post_type='work', days=-20)
         response = self.client.get(reverse('pages:work'))
         self.assertQuerysetEqual(
-            response.context['work_list'],
+            response.context['post_list'],
             ['<ExpPost: Past post 2>', '<ExpPost: Past post 1>'],
             ordered=False
         )
 
-    def test_work_view_with_project_posts(self):
+    def test_list_view_with_future_start_date(self):
         """
-        Project posts should be exluded from work_list.
-        """
-        create_post(title='Project post', post_type='project', days=-10)
-        create_post(title='Work post', post_type='work', days=-10)
-        response = self.client.get(reverse('pages:work'))
-        self.assertQuerysetEqual(
-            response.context['work_list'],
-            ['<ExpPost: Work post>']
-        )
-
-    def test_work_view_with_future_start_date(self):
-        """
-        Jobs that are expected to be started in the future should
+        Jobs and projects that are expected to be started in the future should
         not be posted until they are started.
         """
         create_post(title='Work post', post_type='work', days=-10, start=10)
         response = self.client.get(reverse('pages:work'))
-        self.assertQuerysetEqual(response.context['work_list'], [])
+        self.assertQuerysetEqual(response.context['post_list'], [])
         self.assertContains(response, 'No posts were found.')
 
-class ProjectPostViewTests(TestCase):
-    def test_project_view_with_no_posts(self):
+    def test_list_view_with_different_post_types(self):
         """
-        If no project post exists, an appropriate message should be displayed.
-        """
-        response = self.client.get(reverse('pages:projects'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No projects were found.")
-        self.assertQuerysetEqual(response.context['project_list'], [])
-
-    def test_project_view_with_past_post(self):
-        """
-        Project posts with a pub_date in the past should be displayed on the page.
-        """
-        create_post(title='Past post', post_type='project', days=-10)
-        response = self.client.get(reverse('pages:projects'))
-        self.assertQuerysetEqual(
-            response.context['project_list'],
-            ['<ExpPost: Past post>']
-        )
-
-    def test_project_view_with_future_post(self):
-        """
-        Project posts with a pub_date in the future should not be displayed.
-        """
-        create_post(title='Future post', post_type='project', days=10)
-        response = self.client.get(reverse('pages:projects'))
-        self.assertQuerysetEqual(response.context['project_list'], [])
-        self.assertContains(response, 'No projects were found.')
-    def test_project_view_with_future_and_past_post(self):
-        """
-        Project posts with a pub_date in the future should not be displayed but
-        posts with a pub_date in the past should be displayed.
-        """
-        create_post(title='Future post', post_type='project', days=10)
-        create_post(title='Past post', post_type='project', days=-10)
-        response = self.client.get(reverse('pages:projects'))
-        self.assertQuerysetEqual(
-            response.context['project_list'],
-            ['<ExpPost: Past post>']
-        )
-
-    def test_project_view_with_multiple_past_posts(self):
-        """
-        More than one project post can be displayed at a time as long as the pub_date
+        Both work and job posts should be displayed, as long as the start date
         is in the past.
         """
-        create_post(title='Past post 1', post_type='project', days=-10)
-        create_post(title='Past post 2', post_type='project', days=-20)
-        response = self.client.get(reverse('pages:projects'))
+        create_post(title='Work post', post_type='work')
+        create_post(title='Project post', post_type='project')
+        response = self.client.get(reverse('pages:work'))
         self.assertQuerysetEqual(
-            response.context['project_list'],
-            ['<ExpPost: Past post 2>', '<ExpPost: Past post 1>'],
-            ordered=False
+            response.context['post_list'],
+            ['<ExpPost: Work post>', '<ExpPost: Project post>']
         )
-
-    def test_project_view_with_project_posts(self):
-        """
-        Work posts should be exluded from project_list.
-        """
-        create_post(title='Project post', post_type='project', days=-10)
-        create_post(title='Work post', post_type='work', days=-10)
-        response = self.client.get(reverse('pages:projects'))
-        self.assertQuerysetEqual(
-            response.context['project_list'],
-            ['<ExpPost: Project post>']
-        )
-
-    def test_project_view_with_future_start_date(self):
-        """
-        Projects that are expected to be started in the future should not
-        be posted until they are started.
-        """
-        create_post(title='Project post', post_type='project', days=-10, start=10)
-        response = self.client.get(reverse('pages:projects'))
-        self.assertQuerysetEqual(response.context['project_list'], [])
-        self.assertContains(response, 'No projects were found.')
 
 class ImageModelTests(TestCase):
     def test_post_with_no_images(self):
@@ -208,7 +139,7 @@ class ImageModelTests(TestCase):
         """
         create_post(title='Test post', post_type='work')
         response = self.client.get(reverse('pages:work'))
-        self.assertQuerysetEqual(response.context['work_list'][0].image_set.all(), [])
+        self.assertQuerysetEqual(response.context['post_list'][0].image_set.all(), [])
 
     def test_post_with_image(self):
         """
@@ -216,25 +147,38 @@ class ImageModelTests(TestCase):
         More specific tests to come.
         """
         post = create_post(title='Test post', post_type='work')
-        post.image_set.create(title='Test image', img='test.jpg')
+        post.image_set.create(title='Test image', img='hkg_portrait.png')
         response = self.client.get(reverse('pages:work'))
         self.assertQuerysetEqual(
-            response.context['work_list'][0].image_set.all(),
+            response.context['post_list'][0].image_set.all(),
             ['<Image: Test image>']
         )
 
-class PageViewTests(TestCase):
-    def test_index_view(self):
+class DetailViewTest(TestCase):
+    def test_detail_with_journals(self):
         """
-        Front page should load without errors.
+        Posts containing journals should display journal section.
+
+        post = create_post(title='Test post', post_type='work')
+        journal = Journal(post=post, title='Test journal',
+        pub_date=timezone.now(), author='Henry Gridley', text='test')
+        response = self.client.get(reverse('pages:post_details', post.id))
+        response.assertContains(response, 'Journals')
         """
-        response = self.client.get(reverse('pages:index'))
-        self.assertEqual(response.status_code, 200)
+        pass
 
 class ImageMethodTest(TestCase):
     def test_aspect_ratio(self):
         """
         Returns the aspect ratio of a given image.
         """
-        image = Image(title='Test image', img='test.jpg')
-        self.assertEqual(image.aspect_ratio(), 0.5622188905547226)
+        image = Image(title='Test image', img='hkg_portrait.png')
+        self.assertEqual(image.aspect_ratio(), 0.921875)
+
+class ContactPageTest(TestCase):
+    def test_contact_submit(self):
+        """
+        Contact submit page should display thank you message.
+        """
+        response = self.client.get(reverse('contact_submit'))
+        self.assertContains(response, 'Thank you!')
