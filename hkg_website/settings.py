9a839d01f176c279990513fe284b06e51ef285fe
mcgridles <henry.gridley@gmail.com>
2017-05-21 19:16:28 -0400

    in process of switching to postgresql

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -81,6 +81,15 @@ DATABASES = {
         'PORT': '3306',
         'TIME_ZONE': 'US/Eastern',
     }
+
+    #"default": {
+    #    "ENGINE": "django.db.backends.postgresql_psycopg2",
+    #    "NAME": "[YOUR_DATABASE_NAME]",
+    #    "USER": "[YOUR_USER_NAME]",
+    #    "PASSWORD": "",
+    #    "HOST": "localhost",
+    #    "PORT": "",
+    #}
 }
 
 
