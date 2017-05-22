6619f64c4ffe3ff0ce0344cf9f9654352f361693
mcgridles <henry.gridley@gmail.com>
2017-05-21 22:27:14 -0400

    installed dj-database-url

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -153,3 +154,5 @@ EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 
 HKG_EMAIL = os.environ['HKG_EMAIL']
+
+DATABASES['default'] = dj_database_url.config()
