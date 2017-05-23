754e03e971f09443b798f32eb2032fca495e73d9
mcgridles <henry.gridley@gmail.com>
2017-05-23 13:44:30 -0400

    S3 for media

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-from custom_storage import MediaStorage
+#from custom_storage import MediaStorage
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
