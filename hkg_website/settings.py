d551e3cd6be2f0002796ba8a999a1469f437f160
mcgridles <henry.gridley@gmail.com>
2017-05-22 22:58:50 -0400

    adding S3 support

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 from __future__ import unicode_literals
 import os
 import dj_database_url
-import dj_database_url
+from django.utils import six
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
