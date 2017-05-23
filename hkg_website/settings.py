475b4a550a375f5cf6fd96c2e3ffe020b31af60a
mcgridles <henry.gridley@gmail.com>
2017-05-22 21:51:44 -0400

    fixing static files

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = (
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
