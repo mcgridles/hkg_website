8d36b37cb34878febd8ff90acfb83174e79ce38a
mcgridles <henry.gridley@gmail.com>
2017-05-23 14:15:56 -0400

    S3 for media

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -144,5 +144,5 @@ AWS_S3_REGION_NAME = 'us-east-2'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-MEDIA_URL = 'http://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = 'http://%s.s3.amazonaws.com/%s/media/' % (AWS_STORAGE_BUCKET_NAME, AWS_S3_REGION_NAME)
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
