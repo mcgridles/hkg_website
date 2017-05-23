4b4f7ebf42aa94cad8a2f93eda2b62e39438cd7a
mcgridles <henry.gridley@gmail.com>
2017-05-22 23:27:41 -0400

    replacing S3 with whitenoise

--- hkg_website/conf/dev.py
+++ hkg_website/conf/dev.py
@@ -18,12 +18,12 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "pages/static"),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
