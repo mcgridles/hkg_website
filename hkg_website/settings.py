0c6ba4ffe38b889be6ae9be7d492d08fe455e82d
mcgridles <henry.gridley@gmail.com>
2017-05-25 20:35:56 -0400

    database updated

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'www.henrygridley.me',
@@ -137,13 +137,13 @@ if not DEBUG:
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
     CONN_MAX_AGE = 3600
-    #SECURE_CONTENT_TYPE_NOSNIFF = True
-    #SECURE_BROWSER_XSS_FILTER = True
+    SECURE_CONTENT_TYPE_NOSNIFF = True
+    SECURE_BROWSER_XSS_FILTER = True
+    X_FRAME_OPTIONS = 'DENY'
     #SECURE_SSL_REDIRECT = True
     #SECURE_HSTS_SECONDS = 31536000
     #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    #X_FRAME_OPTIONS = 'DENY'
 
 else:
     ALLOWED_HOSTS += '127.0.0.1',
