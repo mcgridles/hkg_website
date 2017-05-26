53ebebdbde9eef32d7cfc4346a7e91a7c8ea7edc
mcgridles <henry.gridley@gmail.com>
2017-05-25 21:03:06 -0400

    original requirements.txt

--- hkg_website/settings.py
+++ hkg_website/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'www.henrygridley.me',
@@ -116,36 +116,36 @@ EMAIL_PORT = 587
 
 HKG_EMAIL = os.environ['HKG_EMAIL']
 
+#if not DEBUG:
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+STATICFILES_STORAGE = 'hkg_website.custom_storages.StaticStorage'
+DEFAULT_FILE_STORAGE = 'hkg_website.custom_storages.MediaStorage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_HEADERS = {'Cache-Control': 'max-age=86400',}
+AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
+AWS_QUERYSTRING_AUTH = False
+
+# Production security settings
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CONN_MAX_AGE = 3600
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+#SECURE_SSL_REDIRECT = True
+#SECURE_HSTS_SECONDS = 31536000
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 if not DEBUG:
-    DATABASES = {
-        'default': dj_database_url.config()
-    }
-
-    STATICFILES_LOCATION = 'static'
-    MEDIAFILES_LOCATION = 'media'
-
-    STATICFILES_STORAGE = 'hkg_website.custom_storages.StaticStorage'
-    DEFAULT_FILE_STORAGE = 'hkg_website.custom_storages.MediaStorage'
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-    AWS_HEADERS = {'Cache-Control': 'max-age=86400',}
-    AWS_S3_HOST = 's3.us-east-2.amazonaws.com'
-    AWS_QUERYSTRING_AUTH = False
-
-    # Production security settings
-    CSRF_COOKIE_SECURE = True
-    SESSION_COOKIE_SECURE = True
-    CONN_MAX_AGE = 3600
-    SECURE_CONTENT_TYPE_NOSNIFF = True
-    SECURE_BROWSER_XSS_FILTER = True
-    X_FRAME_OPTIONS = 'DENY'
-    #SECURE_SSL_REDIRECT = True
-    #SECURE_HSTS_SECONDS = 31536000
-    #SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    #SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-else:
     ALLOWED_HOSTS += '127.0.0.1',
 
     DATABASES = {
